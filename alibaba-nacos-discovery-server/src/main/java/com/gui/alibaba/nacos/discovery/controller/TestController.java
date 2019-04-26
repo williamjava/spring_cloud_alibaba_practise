6af035b6485190cdb56e02aa7eed709b2bd222c4
wuhoujian <wuhoujian@126.com>
2019-04-26 18:04:19 +0800

    初始化spring cloud alibaba项目

---
+++ alibaba-nacos-discovery-server/src/main/java/com/gui/alibaba/nacos/discovery/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.gui.alibaba.nacos.discovery.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class TestController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam String name) {
+        log.info("invoked name = " + name);
+        return "hello " + name;
+    }
+}
